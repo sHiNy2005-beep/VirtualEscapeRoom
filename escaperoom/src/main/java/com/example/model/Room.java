9a914fc87a865b7af80cb6cd0cc9211e1cfa11d3
CSE-Mashal <mmshami@email.sc.edu>
2025-10-02 13:34:53 -0400

    added #6 and #44

---
+++ escaperoom/src/main/java/com/example/model/Room.java
@@ -0,0 +1,47 @@
+package com.example.model;
+
+import java.util.ArrayList;
+
+public class Room {
+
+    private String roomId;
+    private String title;
+    private String description;
+    private ArrayList<Puzzle> puzzles;
+    private ArrayList<String> items;
+    private String difficulty;
+    private boolean isLocked;
+
+    public Room( String title, String description, String difficulty) {
+        this.title = title;
+        this.description = description;
+        this.difficulty = difficulty;
+        this.puzzles = new ArrayList<>();
+        this.items = new ArrayList<>();
+        this.isLocked = true; 
+    }
+    
+    public void addPuzzle(Puzzle puzzle) {
+        puzzles.add(puzzle);
+    }
+
+    public void addItem(String item) {
+        items.add(item);
+    }
+
+    public void removeItem(String item) {
+        items.remove(item);
+    }
+
+    public ArrayList<String> getItems() {
+        return items;
+    }
+
+    public void unlock() {
+        isLocked = true;
+    }
+
+    public boolean isCompleted() {
+       return true;
+    }
+}
