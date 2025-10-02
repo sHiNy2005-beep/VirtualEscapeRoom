1947ed008150113961f8c0a4bf4e3ec390f71eb2
Murewa_Adebajo <adebajomurewa@gmail.com>
2025-10-02 17:51:37 -0400

    Added #22 and #45

---
+++ escaperoom/src/main/java/com/example/model/GameSession.java
@@ -0,0 +1,50 @@
+package com.example.model;
+import java.util.ArrayList;
+
+
+private String sessionId;
+private Player player;
+private Room room;
+private long startTime;
+private long endTime;
+private int score;
+private int hintsUsed;
+private boolean isCompleted;
+private ArrayList<String> inventory;
+
+
+public GameSession(Player player, Room room) {
+this.player = player;
+this.room = room;
+}
+
+public void startSession() {
+
+}
+
+public void endSession() {
+
+}
+
+public int calculateScore() {
+return score;
+}
+
+public void useHint() {
+
+}
+
+public void collectItem(String item) {
+  
+}
+
+public ArrayList<String> getInventory() {
+return inventory;
+}
+
+public boolean hasItem(String item) {
+if(item == null)
+  return false;
+else
+  return true;
+}
