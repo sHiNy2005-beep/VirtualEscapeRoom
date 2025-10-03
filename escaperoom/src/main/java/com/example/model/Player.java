ec296203ef10209f6595f7e947944d62926f7c65
Murewa_Adebajo <adebajomurewa@gmail.com>
2025-10-03 18:46:32 -0400

    fixed #4, #50, #3, #51, #11, #48, #22, and #45

--- escaperoom/src/main/java/com/example/model/Player.java
+++ escaperoom/src/main/java/com/example/model/Player.java
@@ -6,7 +6,6 @@ public class Player extends User{
     private String username;
     private String email;
     private String password;
-    private ArrayList<String> inventory;
 
     private Player createPlayer(String username, String email, String password) {
         super(username, email, password);
@@ -17,22 +16,22 @@ public class Player extends User{
     }
   
     private void submitAnswer(String answer) {
-        return true; 
+        
     }
 
     private void addItem(String item) {
-      return true;
+      
     }
 
     private void useItem(String item) {
-    return true;
+    
     }
 
     private void useHint() {
-    return null;
+    
     }
 
     private void endGame() {
-    return null;
+    
     }
 }
