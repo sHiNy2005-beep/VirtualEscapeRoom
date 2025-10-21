a74af9bf1eb6660f87dd060276a0ea4310b7560d
CSE-Mashal <mmshami@email.sc.edu>
2025-10-21 09:17:11 -0400

    test speech

--- escaperoom/src/main/java/com/model/EscapeRoomUI.java
+++ escaperoom/src/main/java/com/model/EscapeRoomUI.java
@@ -243,7 +243,8 @@ public class EscapeRoomUI {
     public static void main(String[] args) {
 		EscapeRoomUI EscaperoomInterface = new EscapeRoomUI();
 		EscaperoomInterface.run();
-        Speek.speak("Thank you for playing the Virtual Escape Room! Goodbye!");
+        String welcomeMessage = "Welcome to the Virtual Escape Room!";
+        Speek.speak(welcomeMessage);
 
 	}
    
