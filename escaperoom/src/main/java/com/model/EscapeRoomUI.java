2064a015b21bffdb78dfc24054f067e5e3fd718b
Murewa <adebajomurewa@gmail.com>
2025-10-23 09:48:19 -0400

    Added goal to the text-speech portion of UI

--- escaperoom/src/main/java/com/model/EscapeRoomUI.java
+++ escaperoom/src/main/java/com/model/EscapeRoomUI.java
@@ -18,7 +18,11 @@ public class EscapeRoomUI {
     
 	public void run() {
     System.out.println("\n****Welcome to the Virtual Escape Room!****");
-	scenario1(); //login, list rooms, select room, start game, solve puzzle, end game
+    System.out.println("Goal: ");
+    System.out.println("You must gather evidence of the real culprit by solving puzzles in each room.");
+	System.out.println("Each correctly solved puzzle rewards you with a clue, which points to more than one person.");
+    System.out.println("Gather all the clues, and they will point to one person. Good luck!");
+    scenario1(); //login, list rooms, select room, start game, solve puzzle, end game
 	scenario2();//sign up,select different room, start game, use hint, solve puzzle, end game
     scenario3();//login as different user, list rooms, select room, start game, solve puzzle, end game
     scenario4(); //login, search for an easy room, and check the leaderboard for aformentioned room
@@ -245,6 +249,9 @@ public class EscapeRoomUI {
 		EscapeRoomUI EscaperoomInterface = new EscapeRoomUI();
 		EscaperoomInterface.run();
         Speek.speak("Welcome to the Virtual Escape Room!");
+        Speek.speak("You must gather evidence of the real culprit by solving puzzles in each room.");
+	    Speek.speak("Each correctly solved puzzle rewards you with a clue, which points to more than one person.");
+        Speek.speak("Gather all the clues, and they will point to one person. Good luck!");
 	}
    
 }
