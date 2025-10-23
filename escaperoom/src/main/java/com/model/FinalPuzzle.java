4de4954d503b67d94dc4ee1655df1725ba2006d5
Murewa <adebajomurewa@gmail.com>
2025-10-23 09:22:43 -0400

    Added final puzzle

---
+++ escaperoom/src/main/java/com/model/FinalPuzzle.java
@@ -0,0 +1,15 @@
+package com.model;
+
+public class FinalPuzzle extends Puzzle{
+
+    String solutionValue;
+   
+    public FinalPuzzle(String title, String description, String solution) {
+        super(title, description, solution);
+        this.solutionValue = solution;
+    }  
+    
+
+     
+    
+}
