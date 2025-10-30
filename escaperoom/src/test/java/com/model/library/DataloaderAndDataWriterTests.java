bd588a0ac8f8226af34de7fa81c9152187a5c4d4
CSE-Mashal <mmshami@email.sc.edu>
2025-10-30 09:14:30 -0400

    #105

---
+++ escaperoom/src/test/java/com/model/library/DataloaderAndDataWriterTests.java
@@ -0,0 +1,25 @@
+package com.model.library;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+//Mashal Shami
+
+public class DataloaderAndDataWriterTests {
+    
+     @Test
+    public void dataloaderAndDatawriterTests(){
+        assertTrue(true);
+    }
+
+
+
+
+
+
+
+
+    
+}
