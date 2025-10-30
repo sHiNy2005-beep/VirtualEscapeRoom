bc8b0e592422304e4524b2bad951332f63c982df
Murewa <adebajomurewa@gmail.com>
2025-10-30 14:54:28 -0400

    Started #104, #106 and #109

---
+++ escaperoom/src/test/java/com/model/EscapeRoomFacadeTest.java
@@ -0,0 +1,20 @@
+package com.model;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+//import java.util.ArrayList;
+
+import org.junit.Test;
+
+import com.model.EscapeRoomFacade;
+
+public class EscapeRoomFacadeTest {
+    
+    @Test
+    public void TestTesting() 
+    {
+        assertTrue(true);
+    }
+
+}
---
+++ escaperoom/src/test/java/com/model/PuzzleTest.java
@@ -0,0 +1,62 @@
+package com.model;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+//import java.util.ArrayList;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+import com.model.Puzzle;
+
+public class PuzzleTest {
+    
+    @Test
+    public void TestTesting() 
+    {
+        assertTrue(true);
+    }
+
+    public void checkAnswerTest() 
+    {
+        
+    }
+
+    public void addHintTest() 
+    {
+
+    }
+
+    public void getHintsTest() 
+    {
+
+    }
+
+    public void getTitleTest() 
+    {
+
+    }
+
+    public void getDescriptionTest() 
+    {
+
+    }
+
+    public void isSolvedTest() 
+    {
+
+    }
+
+    public void getSolutionTest() 
+    {
+
+    }
+
+    public void setSolvedTest() 
+    {
+
+    }
+
+}
