84451c8ab74ce04fbee19b795583baa352a34bf3
Murewa <adebajomurewa@gmail.com>
2025-12-03 13:07:50 -0500

    Updated GardenRoom1 fxml

---
+++ escaperoom/src/main/java/com/example/GardenPageController.java
@@ -0,0 +1,61 @@
+package com.example;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.MenuItem;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.shape.Rectangle;
+
+public class GardenPageController {
+
+    @FXML
+    private Button backButton;
+
+    @FXML
+    private Label descLabel;
+
+    @FXML
+    private Label hintLabel;
+
+    @FXML
+    private MenuItem menuHome;
+
+    @FXML
+    private MenuItem menuItems;
+
+    @FXML
+    private MenuItem menuRooms;
+
+    @FXML
+    private Rectangle overlay;
+
+    @FXML
+    private ImageView safeImage;
+
+    @FXML
+    private Label titleLabel;
+
+    @FXML
+    void onMenuHome(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onMenuItems(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onMenuRooms(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onSafeClicked(MouseEvent event) {
+
+    }
+
+}
